91d4519eb73677248287fce1347e7864c30fa538
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-03-15 21:28:36 -0300

    comitt inicial

---
+++ Aula01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+public class Program  {
+
+   public static void Main(){
+      
+      int numero = 10;
+
+      
+      
+   }
+
+}
