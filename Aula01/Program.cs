2226bd5bcfb99051aaf461c2186275299464fca7
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-03-15 22:35:19 -0300

    add .gitignore

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -1,13 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-public class Program  {
+public class Program{
 
    public static void Main(){
-      
-      int numero = 10;
 
-      
-      
+      // int numero = 1;
+      // System.Console.WriteLine(numero);
+      // int retorno;
+      // System.Console.WriteLine(retorno);
+
+// if else
+
+      // if (numero == 10) {
+      //    retorno = alterar(numero);
+      // } else {
+      //    retorno = numero;
+      // }
+      // System.Console.WriteLine(retorno);
+
+// while
+
+      // retorno = alterar(numero);
+
+      // System.Console.WriteLine("Loop while");
+      // while (numero < 10){
+      //    System.Console.WriteLine(retorno);
+      //    numero++;
+      // }
+      // System.Console.WriteLine("");
+
+      // System.Console.WriteLine("Loop for");
+      // for (int i = 0; i<10; i++) {
+      //    System.Console.WriteLine(i);
+      // }
+
+      // int alterar(int num){
+      //    return num + 1;
+      // }
+
+
+// vetor de string
+   //    string [] nomes = new string[]{"Peter", "Clark", "Jason"};
+   //    foreach (string nome in nomes){
+   //       System.Console.WriteLine(nome);
+   //       if (nome == nomes[0]) { 
+   //          break; 
+   //       }
+   //    }
+
+   // }
+
+// switch
+   int var 2;
+   switch (var)
+   {
+      case 3: 
+         System.Console.WriteLine("3");
+         break;
+      default:
+         
    }
 
 }
